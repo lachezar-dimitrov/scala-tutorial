e7bc68b634a886be7bfaf38da4d7409728947a2a
LachezarDimitrov <lachezar_dimitrov@outlook.com>
2021-11-28 10:50:07 +0200

    Initial commit

---
+++ Terms and Types/Method Calls/src/TermsAndTypes.scala
@@ -0,0 +1,16 @@
+object TermsAndTypes {
+
+  def upperCaseMethod(): String = {
+    "Hello, Scala!".toUpperCase()
+  }
+
+  def absMethod(): Int = {
+    -42.abs
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(upperCaseMethod())
+    println(absMethod())
+  }
+
+}
